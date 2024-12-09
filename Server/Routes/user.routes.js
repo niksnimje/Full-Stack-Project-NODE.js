57b3fb75791c21832fd026fc01cbda7399eda4c0
Nikshay Nimje <niksnimje4@gmail.com>
2024-12-09 14:34:12 +0530

    first commit

---
+++ Server/Routes/user.routes.js
@@ -0,0 +1,9 @@
+const express=require("express")
+
+const   userRourtes=express.Router("")
+
+
+
+
+
+module.exports=userRourtes
---
+++ Server/index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const dotenv=require("dotenv")
+dotenv.config()
+const connection = require("./Config/db")
+const userRourtes = require("./Routes/user.routes")
+
+const app=express()
+app.use(express.json())
+
+app.use("/user",userRourtes)
+
+app.listen(process.env.PORT || 3000,async()=>{
+    try {
+        await connection
+        console.log("server is running")
+    } catch (error) {
+        console.log(error)
+    }
+})
