1d97842a6ca7ef77f6e73bdc20fd1d55b02b6b56
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2024-12-13 14:28:21 +0530

    Update notes.routes.js

--- Server/Routes/notes.routes.js
+++ Server/Routes/notes.routes.js
@@ -1,9 +1,12 @@
 const express=require("express")
+const notesCreate = require("../controller/notes.controller")
+const isAuth = require("../Middleware/Auth")
 
-const notesRouter=express.Router()
+const notesRouter=express.Router("")
 
 // Post , Delete , Patch , Update 
 
+notesRouter.post("/create",isAuth,notesCreate)
 
 
 module.exports=notesRouter
