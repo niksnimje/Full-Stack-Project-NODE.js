4831bf3d18efa45fa660728fb353bd5f374b99cb
Nikshay Nimje <148868704+niksnimje@users.noreply.github.com>
2024-12-17 13:55:19 +0530

    Update notes.routes.js

--- Server/Routes/notes.routes.js
+++ Server/Routes/notes.routes.js
@@ -1,7 +1,8 @@
 const express=require("express")
-const {notesCreate, notesDelete, getAllNotesUser,getSingelNotesUser,updateNotes} = require("../controller/notes.controller")
+const {notesCreate, notesDelete, getAllNotesUser,getSingelNotesUser,updateNotes, getAllNotesAdmin} = require("../controller/notes.controller")
 const isAuth = require("../Middleware/Auth")
 const upload = require("../Config/multer")
+const isAdmin = require("../Middleware/Admin")
 
 
 const notesRouter=express.Router("")
@@ -17,6 +18,8 @@ notesRouter.get("/get/:userID",isAuth,getAllNotesUser)
 notesRouter.get("/singelNote/:noteID",isAuth,getSingelNotesUser)
 
 notesRouter.patch("/update/:noteID",isAuth,upload.single("file"),updateNotes)
+
+notesRouter.get("/getAllNotes",isAuth,isAdmin,getAllNotesAdmin)
 
 
 module.exports=notesRouter
